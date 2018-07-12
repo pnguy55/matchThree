c3369970b1d0ebe41f8a3a96ab0252b28ff1de3b
Pnguy55 <pnguy55@gmail.com>
2018-07-12 15:13:22 -0500

    Add movement method to game pieces.

--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -9,25 +9,30 @@ public class GameBoard : MonoBehaviour {
     public int borderSize;
 
     public GameObject tilePrefab;
+    public GameObject[] gamePiecePrefabs;
 
     //Declares a 2d array.  The comma indicates the existance of
     //another dimension in the array
     Tile[,] m_allTiles;
+    GamePiece[,] m_allGamePieces;
 
 	// Use this for initialization
 	void Start () {
         //Instantiates m_allTiles and sets size to the width and height of board.
         m_allTiles = new Tile[width, height];
+        m_allGamePieces = new GamePiece[width, height];
+        
         SetupTiles();
         SetupCamera();
-	}
+        FillRandom();
+    }
 	
 	void SetupTiles()
-        /*
-         * This method utilizes the 2d array m_allTiles to
-         * add tiles 1 by 1 to the board.
-         */
     {
+        /*
+        * This method utilizes the 2d array m_allTiles to
+        * add tiles 1 by 1 to the board.
+        */
         for (int i = 0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
@@ -73,5 +78,63 @@ public class GameBoard : MonoBehaviour {
         float horizontalSize = ((float)width / 2f + (float)borderSize) / aspectRatio;
 
         Camera.main.orthographicSize = (verticalSize > horizontalSize) ? verticalSize : horizontalSize;
+    }
+
+    GameObject GetRandomGamePiece()
+    {
+        //Declares an int to choose a random piece from the 
+        //gamePiecePrefab array.
+        
+        int randomIdx = Random.Range(0, gamePiecePrefabs.Length);
+
+        //Defensive programming, if we forget to set the array in the inspector
+        //This snippet will catch that and give us an error
+        if (gamePiecePrefabs[randomIdx] == null)
+        {
+            Debug.LogWarning("BOARD: " + randomIdx + "does not contain a valid GamePiece prefab!!!");
+        }
+
+        return gamePiecePrefabs[randomIdx];
+    }
+
+    void PlaceGamePiece(GamePiece gamePiece, int x, int y)
+    {
+        //If gamePiece is invalid, it barks at you1
+        if(gamePiece == null)
+        {
+            Debug.LogWarning("BOARD: Invalid GamePiece!!!");
+            return;
+        }
+
+        //Positions the game piece and unrotates and the decided x and y.
+        gamePiece.transform.position = new Vector3(x, y, 0);
+        gamePiece.transform.rotation = Quaternion.identity;
+
+        //Sends position to GamePiece class to store that data.
+        gamePiece.SetCoord(x, y);
+    }
+
+    void FillRandom()
+    {
+        /*Utilizes double loop to populate a 2d array to place gamePieces
+         * in each tile.
+         */
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                //Creates randomPiece gameObject and gives it the piece retrieved from
+                //GetRandomGamePiece function.
+                GameObject randomPiece = Instantiate(GetRandomGamePiece(), Vector3.zero, Quaternion.identity) as GameObject;
+
+                
+                if(randomPiece != null)
+                {
+                    //Passes GamePiece and coordinates to PlaceGamePiece function to populate board
+                    //one by one.
+                    PlaceGamePiece(randomPiece.GetComponent<GamePiece>(), i, j);
+                }
+            }
+        }
     }
 }
